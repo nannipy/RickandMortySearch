1e323ac851a1b0527a7a31ce381e92e3312d014c
nannipy <pernazza.1936239@studenti.uniroma1.it>
2024-07-18 13:07:13 +0200

    Grafica migliorata

--- src/title.js
+++ src/title.js
@@ -1,18 +1,22 @@
-
-<div className='container'>
-<div className='title'>
-    Rick
-    <span> and </span>
-    Morty Character Search
-</div>
-<div className='title middle'>
-    Rick
-    <span> and </span>
-    Morty Character Search
-</div>
-<div className='title bottom'>
-    Rick
-    <span> and </span>
-    Morty Character Search 
-</div>
-</div>
+const Title = () => (
+    
+    <div className='container'>
+    <div className='title'>
+        Rick
+        <span> and </span>
+        Morty Character Search
+    </div>
+    <div className='title middle'>
+        Rick
+        <span> and </span>
+        Morty Character Search
+    </div>
+    <div className='title bottom'>
+        Rick
+        <span> and </span>
+        Morty Character Search 
+    </div>
+    </div>
+      );
+export default Title;
+    
